b107d6a87a2b56f6dd0b3c742acb6e91039d2bde
Guilherme Andrade <g@gandrade.net>
2023-09-17 15:47:44 +0100

    While iterating, quickly detect regressions of working encoding inputs

--- test/sqids_test.exs
+++ test/sqids_test.exs
@@ -3,7 +3,13 @@ defmodule SqidsTest do
 
   doctest Sqids
 
-  test "greets the world" do
-    assert Sqids.hello() == :world
+  test "TEMP: no regressions while iterating" do
+    # Temporary test to quickly check for regressions while iterating
+    {:ok, ctx} = Sqids.new()
+    assert Sqids.encode!(ctx, [1]) == "Uk"
+    assert Sqids.encode!(ctx, [12]) == "vE"
+    assert Sqids.encode!(ctx, [123]) == "UKk"
+    assert Sqids.encode!(ctx, [1234]) == "A4W"
+    assert Sqids.encode!(ctx, [12_345]) == "A6da"
   end
 end
