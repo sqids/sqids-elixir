0ae084dd73b6d820d791386cbc3c53255a002236
Guilherme Andrade <g@gandrade.net>
2023-09-24 18:59:56 +0100

    Fix failing CI

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Sqids.MixProject do
     [
       app: :sqids,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_options: [
